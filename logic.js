a91beca80db16a3960eb272128bce31c2d209da5
mjr390 <mjr117@yahoo.com>
2018-10-15 16:19:18 -0400

    added test circles from hard code and test circles from a list. Referenced a dataset to use for real plot

--- logic.js
+++ logic.js
@@ -14,3 +14,65 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
 
+// size of dot
+function markerSize(population) {
+ return population / 40;
+} 
+  
+
+var meta = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson`
+  d3.json(meta, function(error,eData) {
+    // console.log(eData.features)
+    var dataFeatures = eData.features
+    // console.log(dataFeatures)
+    for (var i = 0; i < dataFeatures.length; i++) {
+        console.log([dataFeatures[i].geometry.coordinates[0], dataFeatures[i].geometry.coordinates[1]])};
+        var plotCircles = [dataFeatures[i].geometry.coordinates[0], dataFeatures[i].geometry.coordinates[1]]
+        console.log(plotCircles)
+
+        // L.circle(plotCircles, {
+        //     fillOpacity: 0.75,
+        //     color: "white",
+        //     fillColor: "purple",
+        //     // Setting our circle's radius equal to the output of our markerSize function
+        //     // This will make our marker's size proportionate to its population
+        //     radius: 20000
+        //   }).addTo(myMap)//.bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+     
+
+});
+
+
+
+
+// var cities = [
+//     {
+//       name: "Los Angeles",
+//       location: [34.0522, -118.2437],
+//       population: 3971883
+//     },
+//     {
+//       name: "Omaha",
+//       location: [41.2524, -95.9980],
+//       population: 446599
+//     }
+//   ];
+// // Loop through the cities array and create one marker for each city object
+// for (var i = 0; i < cities.length; i++) {
+//     L.circle(cities[i].location, {
+//       fillOpacity: 0.75,
+//       color: "white",
+//       fillColor: "purple",
+//       // Setting our circle's radius equal to the output of our markerSize function
+//       // This will make our marker's size proportionate to its population
+//       radius: markerSize(cities[i].population)
+//     }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+//   }
+
+
+// L.circle([45.52, -122.69], {
+//     color: "green",
+//     fillColor: "black",
+//     fillOpacity: 0.75,
+//     radius: 50000
+// }).addTo(myMap);  
